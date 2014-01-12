4fed8025a9240ca0ee84aa78c69f3885c70763c2
kaelzhang <i@kael.me>
2014-01-12 20:14:19 +0800

    adjust start scripts

--- start.sh
+++ start.sh
@@ -7,16 +7,19 @@ STATIC_PORT=9098
 #########################################################
 
 # install npm dependencies
+echo -e "install nodejs environment >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 npm install
 
 # config cortex registry
+echo -e "\nconfigure cortex registry >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 cortex config registry --value http://registry.cortex.dp
 
 # install cortex packages
+echo -e "\ninstall neurons >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 cortex install
 
 # start cortex server
-cortex server &> /dev/null || echo "WARNING: cortex server might be already started"
+cortex server & &> /dev/null || echo "WARNING: cortex server might be already started"
 
 # start static and mock server
-node tools/server.js "$STATIC_PORT"
+node tools/server.js "$STATIC_PORT" &
